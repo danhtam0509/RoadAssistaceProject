f871104e1b6dd115706876fe9521085117797bba
franklintrinh367 <franklintrinh367@gmail.com>
2018-04-20 04:47:58 -0400

    Add files via upload

--- RoadAssistanceProject/AuthenticatedPages/LoginSuccess.aspx.cs
+++ RoadAssistanceProject/AuthenticatedPages/LoginSuccess.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +10,18 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        lblUsername.Text = "" + Session["Username"];
-        Session["Expire"] = true;
-        Session.Clear();
+        using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\PublishProfiles\\Road.mdf;Integrated Security=True"))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT FirstName, LastName FROM Customer WHERE " +
+                "Username='" + Session["Username"] + "'", connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while(reader.Read())
+                    lblUsername.Text = reader.GetString(0);
+            }
+            reader.Close();
+        }
     }
 }
