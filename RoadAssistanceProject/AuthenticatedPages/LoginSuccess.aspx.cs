56aab6594e8ee5199d3d8eb9b9e4a37678d7517b
haobuiii <duyhao0209@gmail.com>
2018-04-20 20:20:48 -0400

    print out the session

--- RoadAssistanceProject/AuthenticatedPages/LoginSuccess.aspx.cs
+++ RoadAssistanceProject/AuthenticatedPages/LoginSuccess.aspx.cs
@@ -10,18 +10,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\PublishProfiles\\Road.mdf;Integrated Security=True"))
-        {
-            SqlCommand cmd = new SqlCommand("SELECT FirstName, LastName FROM Customer WHERE " +
-                "Username='" + Session["Username"] + "'", connection);
-            connection.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                    lblUsername.Text = reader.GetString(0);
-            }
-            reader.Close();
-        }
+        lblUsername.Text = (String) Session["Username"];
     }
 }
