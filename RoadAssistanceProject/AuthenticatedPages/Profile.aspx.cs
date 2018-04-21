bdeeeb7647c43f9d42575583a961916263360e83
Danh Tam Dang <101092895@georgebrown.ca>
2018-04-21 01:09:51 -0400

    Revert "Edit Profile page, database SELECT (read feature)"
    This reverts commit b858409be2f48074931f8fe4e898bd51b18421ec.

--- RoadAssistanceProject/AuthenticatedPages/Profile.aspx.cs
+++ RoadAssistanceProject/AuthenticatedPages/Profile.aspx.cs
@@ -4,38 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Configuration;
-using System.Web.Security;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        // get current user name
-        var userName = HttpContext.Current.User.Identity.Name;
-        // connect to database
-        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
-        conn.Open();
-        string selectData = "SELECT * FROM customer WHERE Username='" + userName + "'";
-        SqlCommand comd = new SqlCommand(selectData, conn);
-        using (SqlDataReader myReader = comd.ExecuteReader())
-        {
-            // while command worked, assign value to Label controls
-            while (myReader.Read())
-            {
-                lblName.Text = myReader["FirstName"] + " " + myReader["LastName"];
-                lblDoB.Text = myReader["DoB"].ToString();
-                lblGender.Text = Convert.ToInt32(myReader["Gender"]) == 0 ? "Male" : "Female";
-                lblAddress.Text = myReader["Address"].ToString();
-                lblEmail.Text = myReader["Email"].ToString();
-                lblPhone.Text = myReader["Phone"].ToString();
-                lblInsurance.Text = myReader["Insurance"].ToString();
-                lblLicense.Text = myReader["License"].ToString();
-            }
-        }
-        // close connection 
-        conn.Close();
 
     }
 
